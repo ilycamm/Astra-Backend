24ba978bab63ada7f52e900fda84dbf734914250
lunarissigma <offahigh200@gmail.com>
2025-07-01 20:08:41 -0700

    Create SetItemFavoriteStatusBatch.ts

---
+++ src/app/mcp/operations/SetItemFavoriteStatusBatch.ts
@@ -0,0 +1,72 @@
+import app from "../../..";
+import Profiles from "../../../db/models/Profiles";
+import { applyProfileChanges } from "../../../utils/handling/applyProfileChanges";
+
+export default function () {
+  app.post(
+    "/fortnite/api/game/v2/profile/:accountId/client/SetItemFavoriteStatusBatch",
+    async (c) => {
+      const profileId = c.req.query("profileId");
+
+      var profiles: any = await Profiles.findOne({
+        accountId: c.req.param("accountId"),
+      });
+      let profile = profiles?.profiles[profileId || "athena"];
+      if (!profile || !profiles) {
+        return c.json({
+          profileRevision: 0,
+          profileId: profileId,
+          profileChangesBaseRevision: 0,
+          profileChanges: [],
+          profileCommandRevision: 0,
+          serverTime: new Date().toISOString(),
+          multiUpdate: [],
+          responseVersion: 1,
+        });
+      }
+
+      let BaseRevision = profile.rvn;
+      let MultiUpdate: any = [];
+      let ApplyProfileChanges: any = [];
+      const body = await c.req.json();
+      const { itemIds, itemFavStatus } = body;
+      console.log(itemIds);
+
+      for (const item of itemIds) {
+        profile.items[item].attributes.favorite = itemFavStatus;
+        ApplyProfileChanges.push({
+          changeType: "itemAttrChanged",
+          item,
+          attributeName: "favorite",
+          attributeValue: profile.items[item].attributes.favorite,
+        });
+      }
+
+      profile.rvn += 1;
+      profile.commandRevision += 1;
+      profile.updated = new Date().toISOString();
+
+      ApplyProfileChanges = [
+        {
+          changeType: "fullProfileUpdate",
+          profile: profile,
+        },
+      ];
+
+      await profiles.updateOne({
+        $set: { [`profiles.${profileId}`]: profile },
+      });
+
+      return c.json({
+        profileRevision: profile.rvn || 0,
+        profileId: profileId,
+        profileChangesBaseRevision: BaseRevision,
+        profileChanges: ApplyProfileChanges,
+        profileCommandRevision: profile.rvn,
+        serverTime: new Date().toISOString(),
+        multiUpdate: MultiUpdate,
+        responseVersion: 1,
+      });
+    }
+  );
+}
